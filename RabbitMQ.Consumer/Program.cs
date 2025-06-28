1d56b5111fb218b8a35a81fbff5d5f1cb369f28b
ahmetbuyukballi <ahmetbuyukballi@gmail.com>
2025-06-28 17:19:20 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+//Bağlantı oluşturma
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+//Bağlantı aktifleştirme ve kanal oluşturmaa
+using IConnection connection=factory.CreateConnection();
+using IModel channel=connection.CreateModel();
+
+//Queue oluşturma
+channel.QueueDeclare(queue: "example", exclusive: false);
+
+//Queueden mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example",false,consumer);
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajın işlendiği yer.
+    //e.body kuyruktaki mesajın verisini getirecektir.
+    //e.body.span veya e.body.toarray() kuyruktaki mesajın byte verisini çeker.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+Console.ReadLine();
